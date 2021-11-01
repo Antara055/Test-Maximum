4720b2a82f2c2d74ea305eac70e38463b8f700ce
Antara055 <antara55.dgp10.1434@gmail.com>
2021-11-01 23:01:38 +0530

    Added : Compare between float numbers

--- src/com/bridgelab/Test_Max_Main.java
+++ src/com/bridgelab/Test_Max_Main.java
@@ -4,6 +4,8 @@ public class Test_Max_Main {
     public static void main(String[] args){
         System.out.println("LET'S FIND THE MAXIMUM NUMBER");
         Test_Max maxNo=new Test_Max(30,50,10);
+        Test_Max maxNoFloat=new Test_Max(30f,50f,10f);
         maxNo.MaximumNumber();
+        maxNoFloat.MaximumNumber();
     }
 }
