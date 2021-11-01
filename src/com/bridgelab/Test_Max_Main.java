8ad3a866502fe7efe4cb17927c99163b6d4fe605
Antara055 <antara55.dgp10.1434@gmail.com>
2021-11-01 23:10:57 +0530

    Added : Compare between three String

--- src/com/bridgelab/Test_Max_Main.java
+++ src/com/bridgelab/Test_Max_Main.java
@@ -5,8 +5,10 @@ public class Test_Max_Main {
         System.out.println("LET'S FIND THE MAXIMUM NUMBER");
         Test_Max <Integer> maxInt=new Test_Max<>(33, -55, 24);
         Test_Max <Float> maxFloat=new Test_Max<>(3.3F, 4.5F, -3.2F);
+        Test_Max<String> maxString=new Test_Max<>("Banana","Apple","Aeach");
 
         maxInt.MaximumNumber();
         maxFloat.MaximumNumber();
+        maxString.MaximumNumber();
     }
 }
