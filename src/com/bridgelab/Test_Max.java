6b14e790ea1b8f66105b61527784d878845439d2
Antara055 <antara55.dgp10.1434@gmail.com>
2021-11-17 13:27:19 +0530

    Added : Generic method

--- src/com/bridgelab/Test_Max.java
+++ src/com/bridgelab/Test_Max.java
@@ -5,19 +5,28 @@ public class Test_Max<T extends Comparable<T>> {
     T a;
     T b;
     T c;
-
+//constructor
     Test_Max(T a, T b, T c) {
         this.a = a;
         this.b = b;
         this.c = c;
     }
+
 //Determines the largest of 3 comparable object
-    public void MaximumNumber() {
-        if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
-            System.out.println("Maximum number is :" + a);
-        } else if (b.compareTo(a) > 0 && b.compareTo(c) > 0) {
-            System.out.println("Maximum number is :" + b);
-        } else
-            System.out.println("Maximum number is :" + c);
+    public <T extends Comparable<T>> T MaximumNumber(T a, T b, T c) {
+        T max=a;
+        if (b.compareTo(max) > 0 ){
+            max=a;
+        }
+        if (c.compareTo(max) > 0 ) {
+            max=c;
+        }
+        printMax(max);
+        return max;
     }
+//printMax generic method to print the maximum value
+    public static <T> void printMax(T max) {
+        System.out.println("Max Value = " + max);
+    }
+
 }
--- src/com/bridgelab/Test_Max_Main.java
+++ src/com/bridgelab/Test_Max_Main.java
@@ -3,12 +3,14 @@ package com.bridgelab;
 public class Test_Max_Main {
     public static void main(String[] args){
         System.out.println("LET'S FIND THE MAXIMUM NUMBER");
+
         Test_Max <Integer> maxInt=new Test_Max<>(33, -55, 24);
         Test_Max <Float> maxFloat=new Test_Max<>(3.3F, 4.5F, -3.2F);
         Test_Max<String> maxString=new Test_Max<>("Banana","Apple","Aeach");
 
-        maxInt.MaximumNumber();
-        maxFloat.MaximumNumber();
-        maxString.MaximumNumber();
+
+        maxInt.MaximumNumber(33, -55, 24);
+        maxFloat.MaximumNumber(3.3F, 4.5F, -3.2F);
+        maxString.MaximumNumber("Banana","Apple","Aeach");
     }
 }
